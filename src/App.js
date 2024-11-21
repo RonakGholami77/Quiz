f3b4224a8083ab9a92c5e4f68ba9b792387f1efa
Rgh <gholamironak88@yahoo.com>
2024-11-21 10:06:08 +0330

    upload quiz project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data";
 
 function App() {
+  const [corse, setCorse] = useState(0);
+  const [indexQ, setIndexQ] = useState(0);
+
+  const finalData = data[indexQ];
+
+  function pickAnswer(isCorrect) {
+    setIndexQ((prev) => {
+      return prev + 1;
+    });
+
+    if (isCorrect) {
+      setCorse((prev) => {
+        const count = prev + 1;
+        return count;
+      });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="quiz-box">
+        {indexQ <= 3 ? (
+          <>
+            <p className="question">{finalData.question}</p>
+            <div className="answers">
+              {finalData.answer.map((answerItem, index) => {
+                return (
+                  <button
+                    key={index}
+                    onClick={() => pickAnswer(answerItem.isCorrect)}
+                  >
+                    {answerItem.label}
+                  </button>
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <p className="result">you got {corse} from 4</p>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const quiz = [
+  {
+    question: "what is the captal of Germany?",
+    answer: [
+      { label: "New Yourk", isCorrect: false },
+      { label: "Berlin", isCorrect: true },
+      { label: "Rom", isCorrect: false },
+      { label: "Tehran", isCorrect: false },
+    ],
+  },
+  {
+    question: "what is the captal of Iran?",
+    answer: [
+      { label: "New Yourk", isCorrect: false },
+      { label: "Berlin", isCorrect: false },
+      { label: "Rom", isCorrect: false },
+      { label: "Tehran", isCorrect: true },
+    ],
+  },
+  {
+    question: "what is the captal of Italy?",
+    answer: [
+      { label: "New Yourk", isCorrect: false },
+      { label: "Berlin", isCorrect: false },
+      { label: "Rom", isCorrect: true },
+      { label: "Tehran", isCorrect: false },
+    ],
+  },
+  {
+    question: "what is the captal of USA?",
+    answer: [
+      { label: "New Yourk", isCorrect: true },
+      { label: "Berlin", isCorrect: false },
+      { label: "Rom", isCorrect: false },
+      { label: "Tehran", isCorrect: false },
+    ],
+  },
+];
+
+export default quiz;
